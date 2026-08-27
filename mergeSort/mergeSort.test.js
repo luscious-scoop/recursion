f1b8fefd9ea85784ded30d20ee90a2bbe6dfd473
Haseeb <ah085904@gmail.com>
2026-08-27 18:02:23 +0500

    chore: define merge sort function and write its tests

---
+++ mergeSort/mergeSort.test.js
@@ -0,0 +1,23 @@
+import { mergeSort } from './mergeSort.js';
+
+test('Sorts an array of length zero ', () => {
+  expect(mergeSort([])).toEqual([]);
+});
+
+test('Sorts an array of length one ', () => {
+  expect(mergeSort([73])).toEqual([73]);
+});
+
+test('Sorts an array of length four ', () => {
+  expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
+});
+
+test('Sorts an array of length five ', () => {
+  expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+});
+
+test('Sorts an array of length eight ', () => {
+  expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([
+    0, 1, 1, 2, 3, 5, 8, 13,
+  ]);
+});
