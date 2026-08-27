a3406b677fc2a2298e35c7a0d977c2a61b645b41
Haseeb <ah085904@gmail.com>
2026-08-27 15:10:24 +0500

    fib(iter): write the iterative function for fibonacci series  and and it's tests

--- fibonacci/fib_iterative/fib_iterative.js
+++ fibonacci/fib_iterative/fib_iterative.js
@@ -1,1 +1,23 @@
-export function fib() {}
+export function fib(n) {
+  let fibonacciNumbers = [0, 1];
+
+  if (n === 1) {
+    return [0];
+  }
+
+  let prev;
+  let next;
+  let value;
+
+  for (let i = 2; i < n; i++) {
+    prev = fibonacciNumbers[i - 1];
+    next = fibonacciNumbers[i - 2];
+    value = prev + next;
+
+    fibonacciNumbers.push(value);
+  }
+
+  return fibonacciNumbers;
+}
+
+// console.log(fib(2));
--- fibonacci/fib_iterative/fib_iterative.test.js
+++ fibonacci/fib_iterative/fib_iterative.test.js
@@ -3,3 +3,22 @@ import { fib } from './fib_iterative.js';
 test('Returns the fibonacci series upto 1', () => {
   expect(fib(1)).toEqual([0]);
 });
+
+test('Returns the fibonacci series upto 2', () => {
+  expect(fib(2)).toEqual([0, 1]);
+});
+
+test('Returns the fibonacci series upto 3', () => {
+  expect(fib(3)).toEqual([0, 1, 1]);
+});
+
+test('Returns the fibonacci series upto 4', () => {
+  expect(fib(4)).toEqual([0, 1, 1, 2]);
+});
+test('Returns the fibonacci series upto 5', () => {
+  expect(fib(5)).toEqual([0, 1, 1, 2, 3]);
+});
+
+test('Returns the fibonacci series upto 8', () => {
+  expect(fib(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
