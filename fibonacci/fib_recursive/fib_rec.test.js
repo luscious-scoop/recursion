fbc2cd260e4182662fc9dce034122178b1c33674
Haseeb <ah085904@gmail.com>
2026-08-27 15:15:02 +0500

    fib(recursive): write the same tests as fib iterative function

---
+++ fibonacci/fib_recursive/fib_rec.test.js
@@ -0,0 +1,24 @@
+import { fibRec } from './fib_rec.js';
+
+test('Returns the fibonacci series upto 1', () => {
+  expect(fibRec(1)).toEqual([0]);
+});
+
+test('Returns the fibonacci series upto 2', () => {
+  expect(fibRec(2)).toEqual([0, 1]);
+});
+
+test('Returns the fibonacci series upto 3', () => {
+  expect(fibRec(3)).toEqual([0, 1, 1]);
+});
+
+test('Returns the fibonacci series upto 4', () => {
+  expect(fibRec(4)).toEqual([0, 1, 1, 2]);
+});
+test('Returns the fibonacci series upto 5', () => {
+  expect(fibRec(5)).toEqual([0, 1, 1, 2, 3]);
+});
+
+test('Returns the fibonacci series upto 8', () => {
+  expect(fibRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
