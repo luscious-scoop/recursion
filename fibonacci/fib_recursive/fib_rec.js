ee745b10e1b861dde0b92254541370254c9b3077
Haseeb <ah085904@gmail.com>
2026-08-27 16:55:38 +0500

    fib(recursive): write function and test to solve fibonacci recursively

--- fibonacci/fib_recursive/fib_rec.js
+++ fibonacci/fib_recursive/fib_rec.js
@@ -1,1 +1,15 @@
-export function fibRec() {}
+export function fibRec(n, fibonacciNumbers = [0], index = 0) {
+  if (n === 1) {
+    return fibonacciNumbers;
+  }
+
+  let prev = fibonacciNumbers[index];
+  let next = fibonacciNumbers[index - 1] ?? 1;
+
+  let value = prev + next;
+
+  fibonacciNumbers.push(value);
+  return fibRec(n - 1, fibonacciNumbers, index + 1);
+}
+
+console.log(fibRec(4));
