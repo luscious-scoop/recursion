6ee14791642a68a8425fd0fc5a3e5dd8251d6935
Haseeb <ah085904@gmail.com>
2026-08-31 17:37:47 +0500

    write a function for merge sort

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,1 +1,34 @@
-export function mergeSort() {}
+export function mergeSort(array) {
+  // base case
+
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let leftArray = mergeSort(array.slice(0, array.length / 2));
+  let rightArray = mergeSort(array.slice(array.length / 2));
+
+  // merge code
+  let i = 0;
+  let j = 0;
+  let result = [];
+
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] < rightArray[j]) {
+      result.push(leftArray[i]);
+      i++;
+    } else {
+      result.push(rightArray[j]);
+      j++;
+    }
+  }
+  for (let k = i; k < leftArray.length; k++) {
+    result.push(leftArray[k]);
+  }
+  for (let z = j; z < rightArray.length; z++) {
+    result.push(rightArray[z]);
+  }
+
+  return result;
+}
+console.log(mergeSort([99.7, 23.3, 8.9, 6.4, 1.5]));
