eb2e7d141b774efa14e43c68ec200eb405a69cb4
Haseeb <ah085904@gmail.com>
2026-08-31 17:36:53 +0500

    test floats too

--- mergeSort/mergeSort.test.js
+++ mergeSort/mergeSort.test.js
@@ -21,3 +21,9 @@ test('Sorts an array of length eight ', () => {
     0, 1, 1, 2, 3, 5, 8, 13,
   ]);
 });
+
+test('sort floats', () => {
+  expect(mergeSort([99.7, 23.3, 8.9, 6.4, 1.5])).toEqual([
+    1.5, 6.4, 8.9, 23.3, 99.7,
+  ]);
+});
